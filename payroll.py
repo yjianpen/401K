5c3d00c1f5ea2c3129827308f1895549eed17bd3
yjianpen <yujianpeng4229@gmail.com>
2022-06-13 13:12:40 -0700

    added some functions like withdraw or deposit to 401K

--- payroll.py
+++ payroll.py
@@ -94,10 +94,13 @@ class roth_account:
 class four_zero_oneK:
 	## param contribution: total contribution to four_zero_oneK in USD
 	## distribution: ratios in pre-tax and roth accounts
-	def __init__(self,contribution,distribution):
-		self.distribution = distribution
-		self.pretax_acc = pretax_account(distribution["pretax"]*contribution)
-		self.roth_acc = roth_account(distribution["roth"]*contribution)
+	def __init__(self,contribution,pretax_part,roth_part):
+		if abs(pretax_part + roth_part - contribution)>1:
+			raise ValueError("Roth balance and pretax balance do not add up to total contribution")
+		self.pretax_acc = pretax_account(pretax_part)
+		self.roth_acc = roth_account(roth_part)
+
+	
 	## To be continued
 
 class taxpayer:
@@ -105,45 +108,76 @@ class taxpayer:
 	def __init__(self,name,status=default_taxpayer_status,salary=0,bonus=0):
 		self.status = status
 		self.name = name
-		self.taxable_income = 0
+		self.salary = 0
 		self.bonus = 0
-		self.salary = salary
+		self.total_income = 0
 
-	def add_total_income(self,income):
-		self.taxable_income += income
 
+	def add_salary(self,income):
+		self.salary += income
+		self.total_income += income
+
+	def add_bonus(self,income):
+		self.bonus += income
+		self.total_income += income
 	def BaseSalaryTax(self,State_tax,Federal_tax,Fica_tax):
 		tax_paid = 0
 		if self.status["Resident"] == "True":
-			tax_paid+=Fica_tax.CountTax(self.taxable_income)
+			tax_paid+=Fica_tax.CountTax(self.salary)
 
-		tax_paid+=State_tax.CountTax(self.taxable_income)
+		tax_paid+=State_tax.CountTax(self.salary)
 
-		tax_paid+=Federal_tax.CountTax(self.taxable_income)
+		tax_paid+=Federal_tax.CountTax(self.salary)
 
-		print("Base salary paid tax: ",tax_paid,"Base salary tax rate: ",tax_paid/self.taxable_income)
+		print("Base salary paid tax: ",tax_paid,"Base salary tax rate: ",tax_paid/self.salary)
 
 		return tax_paid
 
-	def BonusTax(self):
+
+	def BonusTax(self,State_tax,Federal_tax,Fica_tax):
+		tax_paid = 0
+		if self.status["Resident"] == "True":
+			tax_paid+=Fica_tax.CountTax(self.bonus)
+
+		tax_paid+=State_tax.CountTax(self.bonus)
+
+		tax_paid+=Federal_tax.CountTax(self.bonus)
+
+		print("Bonus paid tax: ",tax_paid,"Base bonus tax rate: ",tax_paid/self.salary)
+
+		return tax_paid
+
+	def CountTax(self,State_salary_tax,Federal_salary_tax,Fica_tax,State_bonus_tax,Federal_bonus_tax):
+		tax_paid = 0
+		tax_paid += self.BaseSalaryTax(State_salary_tax,Federal_salary_tax,Fica_tax)
+		tax_paid += self.BonusTax(State_bonus_tax,Federal_bonus_tax,Fica_tax)
+		print("Overall paid tax: ",tax_paid,"Overall tax rate: ",tax_paid/(self.salary+self.bonus))
+		return tax_paid
+
+
+	def deposit401K(self,from_account,to_account):
 		'''
 		To be continued
 		'''
-		return
+		valid_from_accounts = ["salary","bonus"]
+		valid_to_accounts = ["pretax","roth"]
+		if from_account not in valid_from_accounts:
+			raise ValueError("from_account must be listed here: ",valid_from_account)
+		if to_account not in valid_to_accounts:
+			raise ValueError("to_account must be listed here: ",valid_to_account)
 
-	def deposit401K(self):
-		'''
-		To be continued
-		'''
 		return
-
+Federal_bonus_tax=tax_cumulative({(0,100000):0.22,(100000,float('inf')):0.37},"Federal_bonus_tax")
+Ca_bonus_tax=tax_flat(0.1023,"Ca_bonus_tax")
+Wa_income_tax= tax_cumulative({(0,float('inf')):0},"Wa base salary tax")
 SS_income_tax = tax_cumulative({(0,147000):0.062,(147001,float('inf')):0},"Social Security base salary tax")
 Medicare_income_tax = tax_cumulative({(0,200000):0.0145,(200001,float('inf')):0.009},"Medicare base salary tax")
 Federal_income_tax = tax_cumulative({(0,9950):0.1,(9951,40525):0.12,(40526,86375):0.22,(86376,164925):0.24,(164926,209425):0.32,(209416,523600):0.35,(523601,float('inf')):0.37},"Fed base salary tax")
 Ca_income_tax= tax_cumulative({(0,9325):0.01,(9326,22107):0.02,(22108,34892):0.04,(34893,48435):0.06,(48436,61214):0.08,(61215,212686):0.093,(312687,375221):0.103,(375222,625369):0.113,(625370,float('inf')):0.123},"Ca base salary tax")
 Fica_income_tax = tax_combined([SS_income_tax,Medicare_income_tax])
 taxpayer1 = taxpayer("Gavin")
-taxpayer1.add_total_income(170000)
-taxpayer1.BaseSalaryTax(Federal_income_tax,Ca_income_tax,Fica_income_tax)
+taxpayer1.add_salary(152000)
+taxpayer1.add_bonus(51000)
+taxpayer1.CountTax(Federal_income_tax,Ca_income_tax,Fica_income_tax,Ca_bonus_tax,Federal_bonus_tax)
 ##print(Federal_tax.CountTax(200000))
 ##print(Ca_tax.CountTax(200000))
